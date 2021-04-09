e434bd505d1d1ce6e983c0c5cb05edb3db77b95b
Guilherme Rodrigues Tafelli da Silva <38620644+GuilhermeTafelli@users.noreply.github.com>
2021-04-08 21:43:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -29,10 +29,11 @@ app.get("/keila-neves", async (req, res) => {
         "https://api.whatsapp.com/send?phone=5511989548529",
         "https://api.whatsapp.com/send?phone=5547999202315",
         "https://api.whatsapp.com/send?phone=5511989548529",
+        "https://api.whatsapp.com/send?phone=5567996740449",
         "https://api.whatsapp.com/send?phone=5511992338844"
     ]
 
-    res.redirect(urlList[countOld.count%6])
+    res.redirect(urlList[countOld.count%9])
 });
 
 
